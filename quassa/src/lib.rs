8197fd16159d0021f01f685aebf7e51b84c9149e
segfreak <segfreak@gmail.com>
2026-04-29 17:46:21 +0300

    fix: quassa::prelude, default features = ["hashbrown"]

--- quassa/src/lib.rs
+++ quassa/src/lib.rs
@@ -4,13 +4,13 @@ pub mod target;
 pub mod verify;
 
 pub mod prelude {
-    #[cfg(feature = "hashbrown")]
-    pub use hashbrown::HashMap;
     #[cfg(not(feature = "hashbrown"))]
     pub use std::collections::HashMap;
-
-    #[cfg(feature = "hashbrown")]
-    pub use hashbrown::HashSet;
     #[cfg(not(feature = "hashbrown"))]
     pub use std::collections::HashSet;
+
+    #[cfg(feature = "hashbrown")]
+    pub use hashbrown::HashMap;
+    #[cfg(feature = "hashbrown")]
+    pub use hashbrown::HashSet;
 }
