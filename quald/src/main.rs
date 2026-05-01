52fd9dce08180cf23833ab20d8de092ed24b0880
segfreak <segfreak@gmail.com>
2026-05-01 15:56:08 +0300

    ld: +elf parser

--- quald/src/main.rs
+++ quald/src/main.rs
@@ -1,3 +1,25 @@
-fn main() {
-    todo!("quald linker not implemented yet")
+use std::{fs::File, io};
+
+use memmap2::Mmap;
+use quald::elf::Elf;
+
+pub fn map_file(path: &str) -> io::Result<Mmap> {
+    let file = File::open(path)?;
+    unsafe { Mmap::map(&file) }
+}
+
+fn main() -> std::io::Result<()> {
+    let mmap = map_file("a.out")?;
+    let elf = Elf::parse(&mmap)?;
+
+    println!("entry: 0x{:x}", elf.ehdr.e_entry);
+
+    for seg in elf.find_load_segments() {
+        println!(
+            "LOAD offset=0x{:x} vaddr=0x{:x} size=0x{:x}",
+            seg.p_offset, seg.p_vaddr, seg.p_filesz
+        );
+    }
+
+    Ok(())
 }
